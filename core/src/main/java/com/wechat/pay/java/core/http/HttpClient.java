188d7fce386ab9084d70203151029a31c21c9d74
Xiaoyu PENG <peng.xiaoyu@gmail.com>
2022-07-21 19:36:26 +0800

    refactor: 将 get/post/patch 等实现移到接口 HttpClient 中作为默认方法 (#13)

--- core/src/main/java/com/wechat/pay/java/core/http/HttpClient.java
+++ core/src/main/java/com/wechat/pay/java/core/http/HttpClient.java
@@ -35,7 +35,11 @@ public interface HttpClient {
    * @throws ServiceException 发送HTTP请求成功，服务返回异常。例如返回状态码小于200或大于等于300。
    * @throws ParseException 服务返回成功，content-type不为application/json、解析返回体失败。
    */
-  <T> HttpResponse<T> get(HttpHeaders headers, String url, Class<T> responseClass);
+  default <T> HttpResponse<T> get(HttpHeaders headers, String url, Class<T> responseClass) {
+    HttpRequest httpRequest =
+        new HttpRequest.Builder().url(url).httpMethod(HttpMethod.GET).headers(headers).build();
+    return execute(httpRequest, responseClass);
+  }
 
   /**
    * 发送POST请求
@@ -51,8 +55,17 @@ public interface HttpClient {
    * @throws ServiceException 发送HTTP请求成功，服务返回异常。例如返回状态码小于200或大于等于300。
    * @throws ParseException 服务返回成功，content-type不为application/json、解析返回体失败。
    */
-  <T> HttpResponse<T> post(
-      HttpHeaders headers, String url, RequestBody body, Class<T> responseClass);
+  default <T> HttpResponse<T> post(
+      HttpHeaders headers, String url, RequestBody body, Class<T> responseClass) {
+    HttpRequest httpRequest =
+        new HttpRequest.Builder()
+            .url(url)
+            .httpMethod(HttpMethod.POST)
+            .headers(headers)
+            .body(body)
+            .build();
+    return execute(httpRequest, responseClass);
+  }
 
   /**
    * 发送PATCH请求
@@ -68,8 +81,17 @@ public interface HttpClient {
    * @throws ServiceException 发送HTTP请求成功，服务返回异常。例如返回状态码小于200或大于等于300。
    * @throws ParseException 服务返回成功，content-type不为application/json、解析返回体失败。
    */
-  <T> HttpResponse<T> patch(
-      HttpHeaders headers, String url, RequestBody body, Class<T> responseClass);
+  default <T> HttpResponse<T> patch(
+      HttpHeaders headers, String url, RequestBody body, Class<T> responseClass) {
+    HttpRequest httpRequest =
+        new HttpRequest.Builder()
+            .url(url)
+            .httpMethod(HttpMethod.PATCH)
+            .headers(headers)
+            .body(body)
+            .build();
+    return execute(httpRequest, responseClass);
+  }
 
   /**
    * 发送PUT请求
@@ -85,8 +107,17 @@ public interface HttpClient {
    * @throws ServiceException 发送HTTP请求成功，服务返回异常。例如返回状态码小于200或大于等于300。
    * @throws ParseException 服务返回成功，content-type不为application/json、解析返回体失败。
    */
-  <T> HttpResponse<T> put(
-      HttpHeaders headers, String url, RequestBody body, Class<T> responseClass);
+  default <T> HttpResponse<T> put(
+      HttpHeaders headers, String url, RequestBody body, Class<T> responseClass) {
+    HttpRequest httpRequest =
+        new HttpRequest.Builder()
+            .url(url)
+            .httpMethod(HttpMethod.PUT)
+            .headers(headers)
+            .body(body)
+            .build();
+    return execute(httpRequest, responseClass);
+  }
 
   /**
    * 发送DELETE请求
@@ -101,5 +132,9 @@ public interface HttpClient {
    * @throws ServiceException 发送HTTP请求成功，服务返回异常。例如返回状态码小于200或大于等于300。
    * @throws ParseException 服务返回成功，content-type不为application/json、解析返回体失败。
    */
-  <T> HttpResponse<T> delete(HttpHeaders headers, String url, Class<T> responseClass);
+  default <T> HttpResponse<T> delete(HttpHeaders headers, String url, Class<T> responseClass) {
+    HttpRequest httpRequest =
+        new HttpRequest.Builder().url(url).httpMethod(HttpMethod.DELETE).headers(headers).build();
+    return execute(httpRequest, responseClass);
+  }
 }
