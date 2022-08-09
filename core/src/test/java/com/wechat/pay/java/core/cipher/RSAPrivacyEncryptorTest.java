ae24d51935d4c30c57c20d90043dec1f1a8f92ee
Xiaoyu PENG <peng.xiaoyu@gmail.com>
2022-08-09 10:58:23 +0800

    feat: 新增解密失败异常 refactor: 敏感信息加密基于jca实现抽象类，并把RSA-OAEP和RSA-PKCS1拆分成两个子类

--- core/src/test/java/com/wechat/pay/java/core/cipher/RSAPrivacyEncryptorTest.java
+++ core/src/test/java/com/wechat/pay/java/core/cipher/RSAPrivacyEncryptorTest.java
@@ -11,8 +11,6 @@ public class RSAPrivacyEncryptorTest {
   private static RSAPrivacyEncryptor rsaPrivacyEncryptor;
   private static RSAPrivacyDecryptor rsaPrivacyDecryptor;
   private static final String PLAINTEXT = "plaintext";
-  private static final String RSA_OAEP = "RSA/ECB/OAEPWithSHA-1AndMGF1Padding";
-  private static final String RSA_PKCS1v15 = "RSA/ECB/PKCS1Padding";
 
   @BeforeClass
   public static void init() {
@@ -23,7 +21,7 @@ public class RSAPrivacyEncryptorTest {
   }
 
   @Test
-  public void testEncrypt() {
+  public void testEncryptWithOAEP() {
     String ciphertext = rsaPrivacyEncryptor.encrypt(PLAINTEXT);
     String decryptMessage = rsaPrivacyDecryptor.decrypt(ciphertext);
     Assert.assertEquals(PLAINTEXT, decryptMessage);
@@ -31,17 +29,8 @@ public class RSAPrivacyEncryptorTest {
         MERCHANT_CERTIFICATE_SERIAL_NUMBER, rsaPrivacyEncryptor.getWechatpaySerial());
   }
 
-  @Test
-  public void testEncryptWithOAEP() {
-    String ciphertext = rsaPrivacyEncryptor.encryptWithTransformation(RSA_OAEP, PLAINTEXT);
-    String decryptMessage = rsaPrivacyDecryptor.decryptWithTransformation(RSA_OAEP, ciphertext);
-    Assert.assertEquals(PLAINTEXT, decryptMessage);
-  }
-
-  @Test
-  public void testEncryptWithPKCS1v15() {
-    String ciphertext = rsaPrivacyEncryptor.encryptWithTransformation(RSA_PKCS1v15, PLAINTEXT);
-    String decryptMessage = rsaPrivacyDecryptor.decryptWithTransformation(RSA_PKCS1v15, ciphertext);
-    Assert.assertEquals(PLAINTEXT, decryptMessage);
+  @Test(expected = IllegalArgumentException.class)
+  public void testEncryptTooLargePlaintext() {
+    rsaPrivacyEncryptor.encrypt(new String(new char[220]));
   }
 }
