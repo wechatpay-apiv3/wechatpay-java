5f3ebe3e5480db050531c5ef9ee7603906e231e0
lianup <769025704@qq.com>
2022-09-23 17:21:33 +0800

    test:补充测试用例

--- core/src/test/java/com/wechat/pay/java/core/util/StringUtilTest.java
+++ core/src/test/java/com/wechat/pay/java/core/util/StringUtilTest.java
@@ -1,15 +1,21 @@
 package com.wechat.pay.java.core.util;
 
 import static com.wechat.pay.java.core.util.StringUtil.toIndentedString;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import com.wechat.pay.java.core.model.TestServiceRequest;
 import org.junit.jupiter.api.Test;
 
-public class StringUtilTest {
+class StringUtilTest {
 
   @Test
-  public void testToIndentedString() {
+  void testNullObject() {
+    assertEquals("null", toIndentedString(null));
+  }
+
+  @Test
+  void testToIndentedStringSucc() {
     TestServiceRequest request = new TestServiceRequest();
     request.setAppid("appid");
     request.setMchid("mchid");
