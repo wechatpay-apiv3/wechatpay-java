ee87a1e785fda35b8dc28a5f9c85baf10af4cf67
lianup <769025704@qq.com>
2022-09-27 16:41:35 +0800

    docs:完善example文档

--- service/src/example/java/com/wechat/pay/java/service/refund/RefundServiceExample.java
+++ service/src/example/java/com/wechat/pay/java/service/refund/RefundServiceExample.java
@@ -1,6 +1,9 @@
 package com.wechat.pay.java.service.refund;
 
 import com.wechat.pay.java.core.RSAConfig;
+import com.wechat.pay.java.core.exception.HttpException;
+import com.wechat.pay.java.core.exception.MalformedMessageException;
+import com.wechat.pay.java.core.exception.ServiceException;
 import com.wechat.pay.java.service.refund.model.CreateRequest;
 import com.wechat.pay.java.service.refund.model.QueryByOutRefundNoRequest;
 import com.wechat.pay.java.service.refund.model.Refund;
@@ -28,16 +31,29 @@ public class RefundServiceExample {
     // 初始化服务
     service = new RefundService.Builder().config(config).build();
     // ... 调用接口
+    try {
+      Refund response = create();
+    } catch (HttpException e) { // 发送HTTP请求失败
+      // 调用e.getHttpRequest()获取请求打印日志或上报监控，更多方法见HttpException定义
+    } catch (ServiceException e) { // 服务返回状态小于200或大于等于300，例如500
+      // 调用e.getResponseBody()获取返回体打印日志或上报监控，更多方法见ServiceException定义
+    } catch (MalformedMessageException e) { // 服务返回成功，返回体类型不合法，或者解析返回体失败
+      // 调用e.getMessage()获取信息打印日志或上报监控，更多方法见MalformedMessageException定义
+    }
   }
   /** 退款申请 */
   public static Refund create() {
     CreateRequest request = new CreateRequest();
+    // 调用request.setXxx(val)设置所需参数，具体参数可见Request定义
+    // 调用接口
     return service.create(request);
   }
   /** 查询单笔退款（通过商户退款单号） */
   public static Refund queryByOutRefundNo() {
 
     QueryByOutRefundNoRequest request = new QueryByOutRefundNoRequest();
+    // 调用request.setXxx(val)设置所需参数，具体参数可见Request定义
+    // 调用接口
     return service.queryByOutRefundNo(request);
   }
 }
