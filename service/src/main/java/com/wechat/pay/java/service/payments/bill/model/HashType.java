817cb87f3fb567d1bab1d56980604017236fbac7
lianup <769025704@qq.com>
2022-12-16 15:58:33 +0800

    refactor:去除BillService

--- service/src/main/java/com/wechat/pay/java/service/payments/bill/model/HashType.java
+++
@@ -1,9 +0,0 @@
-package com.wechat.pay.java.service.payments.bill.model;
-
-import com.google.gson.annotations.SerializedName;
-
-/** 哈希类型 */
-public enum HashType {
-  @SerializedName("SHA1")
-  SHA1
-}
--- service/src/main/java/com/wechat/pay/java/service/payments/bill/model/TarType.java
+++
@@ -1,9 +0,0 @@
-package com.wechat.pay.java.service.payments.bill.model;
-
-import com.google.gson.annotations.SerializedName;
-
-/** 压缩类型 GZIP：返回格式为.gzip的压缩包账单 */
-public enum TarType {
-  @SerializedName("GZIP")
-  GZIP
-}
