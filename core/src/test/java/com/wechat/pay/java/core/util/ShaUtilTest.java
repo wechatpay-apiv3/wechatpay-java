fadca5ef20fbca674364ad1d5b7895ba799e8cca
Xiaoyu PENG <peng.xiaoyu@gmail.com>
2023-02-20 16:11:27 +0800

    feat: 增加 `getSha1HexString(byte[])`, resolve #126

--- core/src/test/java/com/wechat/pay/java/core/util/ShaUtilTest.java
+++ core/src/test/java/com/wechat/pay/java/core/util/ShaUtilTest.java
@@ -9,13 +9,16 @@ import org.junit.Test;
 public class ShaUtilTest {
 
   private static final String MESSAGE = "message";
+  private static final String SHA1 = "6f9b9af3cd6e8b8a73c2cdced37fe9f59226e27d";
+  private static final String SHA256 =
+      "ab530a13e45914982b79f9b7e3fba994cfd1f3fb22f71cea1afbf02b460c6d1d";
 
   @Test
   public void testInputStreamGetSha1HexString() throws IOException {
     ByteArrayInputStream inputStream =
         new ByteArrayInputStream(MESSAGE.getBytes(StandardCharsets.UTF_8));
     String sha = ShaUtil.getSha1HexString(inputStream);
-    Assert.assertNotNull(sha);
+    Assert.assertEquals(SHA1, sha);
   }
 
   @Test
@@ -23,12 +26,18 @@ public class ShaUtilTest {
     ByteArrayInputStream inputStream =
         new ByteArrayInputStream(MESSAGE.getBytes(StandardCharsets.UTF_8));
     String sha = ShaUtil.getSha256HexString(inputStream);
-    Assert.assertNotNull(sha);
+    Assert.assertEquals(SHA256, sha);
   }
 
   @Test
   public void testBytesGetSha256HexString() {
     String sha = ShaUtil.getSha256HexString(MESSAGE.getBytes(StandardCharsets.UTF_8));
-    Assert.assertNotNull(sha);
+    Assert.assertEquals(SHA256, sha);
+  }
+
+  @Test
+  public void testBytesGetSha1HexString() {
+    String sha = ShaUtil.getSha1HexString(MESSAGE.getBytes(StandardCharsets.UTF_8));
+    Assert.assertEquals(SHA1, sha);
   }
 }
