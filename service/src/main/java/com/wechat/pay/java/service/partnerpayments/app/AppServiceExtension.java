23c3e9e289fd190975fb3708b3029f204658a1ae
Xiaoyu PENG <peng.xiaoyu@gmail.com>
2023-02-20 14:40:33 +0800

    docs: 补充 JsapiServiceExtension/AppServiceExtension, resolve #137

--- service/src/main/java/com/wechat/pay/java/service/partnerpayments/app/AppServiceExtension.java
+++ service/src/main/java/com/wechat/pay/java/service/partnerpayments/app/AppServiceExtension.java
@@ -19,6 +19,11 @@ import java.time.Instant;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * APP 支付的扩展类。
+ *
+ * <p>它封装了 AppService，并提供了一个增强的 APP 下单方法 prepayWithRequestPayment。
+ */
 public class AppServiceExtension {
   private final Signer signer;
   private final AppService appService;
@@ -36,11 +41,14 @@ public class AppServiceExtension {
     this.appService = builder.build();
   }
   /**
-   * APP支付下单，并返回APP调起支付数据
+   * APP 支付下单，并返回 APP 调起支付数据。推荐使用！
+   *
+   * <p>请求成功后，该方法返回预支付交易会话标识 prepay_id 和客户端 APP 调起支付所需参数。
+   * 它相比 AppService.prepay 更简单易用，因为无需开发者自行计算调起支付签名。
    *
    * @param request 请求参数
    * @param requestPaymentAppid 商户申请的公众号对应的appid
-   * @return PrepayResponse
+   * @return PrepayWithRequestPaymentResponse
    * @throws HttpException 发送HTTP请求失败。例如构建请求参数失败、发送请求失败、I/O错误等。包含请求信息。
    * @throws ValidationException 发送HTTP请求成功，验证微信支付返回签名失败。
    * @throws ServiceException 发送HTTP请求成功，服务返回异常。例如返回状态码小于200或大于等于300。
