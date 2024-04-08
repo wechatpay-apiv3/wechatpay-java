be03b4f6c792cc9db1785f4e60a2b58445ed42f1
jeanwu <jeanwu@tencent.com>
2024-04-08 21:48:21 +0800

    优化代码

--- core/src/main/java/com/wechat/pay/java/core/certificate/RSACertificateHandler.java
+++ core/src/main/java/com/wechat/pay/java/core/certificate/RSACertificateHandler.java
@@ -13,6 +13,5 @@ final class RSACertificateHandler implements CertificateHandler {
   @Override
   public void validateCertPath(X509Certificate certificate) {
     // 为防止证书过期导致验签失败，从而影响业务，后续不再验证证书信任链
-    return;
   }
 }
