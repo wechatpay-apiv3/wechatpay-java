504f78c6ce000abaca71189d5483d28ce6da928b
jeanwu <513564656@qq.com>
2024-05-07 10:20:50 +0800

    回滚代码

--- core/src/test/java/com/wechat/pay/java/core/notification/RSANotificationConfigTest.java
+++ core/src/test/java/com/wechat/pay/java/core/notification/RSANotificationConfigTest.java
@@ -3,11 +3,7 @@ package com.wechat.pay.java.core.notification;
 import static com.wechat.pay.java.core.model.TestConfig.API_V3_KEY;
 import static com.wechat.pay.java.core.model.TestConfig.WECHAT_PAY_CERTIFICATE;
 import static com.wechat.pay.java.core.model.TestConfig.WECHAT_PAY_CERTIFICATE_PATH;
-import static com.wechat.pay.java.core.model.TestConfig.WECHAT_PAY_CERTIFICATE_SERIAL_NUMBER;
 import static com.wechat.pay.java.core.model.TestConfig.WECHAT_PAY_CERTIFICATE_STRING;
-import static com.wechat.pay.java.core.model.TestConfig.WECHAT_PAY_PUBLIC_KEY;
-import static com.wechat.pay.java.core.model.TestConfig.WECHAT_PAY_PUBLIC_KEY_PATH;
-import static com.wechat.pay.java.core.model.TestConfig.WECHAT_PAY_PUBLIC_KEY_STRING;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -43,33 +39,15 @@ class RSANotificationConfigTest implements NotificationConfigTest {
 
   static Stream<Builder> BuilderProvider() {
     return Stream.of(
-        // certificates from string
+        // from string
         new RSANotificationConfig.Builder()
             .apiV3Key(API_V3_KEY)
             .certificates(WECHAT_PAY_CERTIFICATE),
 
-        // certificates form path
+        // form path
         new RSANotificationConfig.Builder()
             .apiV3Key(API_V3_KEY)
-            .certificatesFromPath(WECHAT_PAY_CERTIFICATE_PATH),
-
-        // publickey form string
-        new RSANotificationConfig.Builder()
-            .apiV3Key(API_V3_KEY)
-            .publicKey(WECHAT_PAY_PUBLIC_KEY_STRING)
-            .publicKeyId(WECHAT_PAY_CERTIFICATE_SERIAL_NUMBER),
-
-        // publickey form path
-        new RSANotificationConfig.Builder()
-            .apiV3Key(API_V3_KEY)
-            .publicFromPath(WECHAT_PAY_PUBLIC_KEY_PATH)
-            .publicKeyId(WECHAT_PAY_CERTIFICATE_SERIAL_NUMBER),
-
-        // publickey
-        new RSANotificationConfig.Builder()
-            .apiV3Key(API_V3_KEY)
-            .publicKey(WECHAT_PAY_PUBLIC_KEY)
-            .publicKeyId(WECHAT_PAY_CERTIFICATE_SERIAL_NUMBER));
+            .certificatesFromPath(WECHAT_PAY_CERTIFICATE_PATH));
   }
 
   @Test
