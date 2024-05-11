4c235d186db8b082165d5f11b672ae4b8a42d42b
jeanwu <513564656@qq.com>
2024-05-11 16:31:01 +0800

    回调和回包验签支持使用平台公钥

--- core/src/main/java/com/wechat/pay/java/core/certificate/CertificateHandler.java
+++ core/src/main/java/com/wechat/pay/java/core/certificate/CertificateHandler.java
@@ -14,7 +14,7 @@ public interface CertificateHandler {
   X509Certificate generateCertificate(String certificate);
 
   /**
-   * * 验证证书链
+   * * 验证证书链（不推荐验证，如果证书过期不及时更换会导致验证失败，从而影响业务）
    *
    * @param certificate 微信支付平台证书
    * @throws com.wechat.pay.java.core.exception.ValidationException 证书验证失败
--- core/src/main/java/com/wechat/pay/java/core/cipher/RSAVerifier.java
+++ core/src/main/java/com/wechat/pay/java/core/cipher/RSAVerifier.java
@@ -3,11 +3,16 @@ package com.wechat.pay.java.core.cipher;
 import static com.wechat.pay.java.core.cipher.Constant.SHA256WITHRSA;
 
 import com.wechat.pay.java.core.certificate.CertificateProvider;
+import java.security.PublicKey;
 
 /** RSA验签器 */
 public final class RSAVerifier extends AbstractVerifier {
 
   public RSAVerifier(CertificateProvider provider) {
     super(SHA256WITHRSA, provider);
+  }
+
+  public RSAVerifier(PublicKey publicKey, String publicKeyId) {
+    super(SHA256WITHRSA, publicKey, publicKeyId);
   }
 }
