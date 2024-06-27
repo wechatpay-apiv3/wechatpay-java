f9e71b44113b8d21e7409062d8de3acbb8aa42e4
刘李鹏 <liu.lipeng@icloud.com>
2024-06-27 11:21:38 +0800

    收付通合并支付相关接口对接实现(#282)

---
+++ service/src/example/java/com/wechat/pay/java/service/ecommercecombinepayments/h5/H5ServiceExample.java
@@ -0,0 +1,77 @@
+package com.wechat.pay.java.service.ecommercecombinepayments.h5;
+
+import com.wechat.pay.java.core.Config;
+import com.wechat.pay.java.core.RSAAutoCertificateConfig;
+import com.wechat.pay.java.core.exception.HttpException;
+import com.wechat.pay.java.core.exception.MalformedMessageException;
+import com.wechat.pay.java.core.exception.ServiceException;
+import com.wechat.pay.java.service.ecommercecombinepayments.h5.model.*;
+
+/** H5Service使用示例 */
+public class H5ServiceExample {
+
+  /** 商户号 */
+  public static String merchantId = "190000****";
+
+  /** 商户API私钥路径 */
+  public static String privateKeyPath = "/Users/yourname/your/path/apiclient_key.pem";
+
+  /** 商户证书序列号 */
+  public static String merchantSerialNumber = "5157F09EFDC096DE15EBE81A47057A72********";
+
+  /** 商户APIV3密钥 */
+  public static String apiV3Key = "...";
+
+  public static H5Service service;
+
+  public static void main(String[] args) {
+    // 初始化商户配置
+    Config config =
+        new RSAAutoCertificateConfig.Builder()
+            .merchantId(merchantId)
+            // 使用 com.wechat.pay.java.core.util 中的函数从本地文件中加载商户私钥，商户私钥会用来生成请求的签名
+            .privateKeyFromPath(privateKeyPath)
+            .merchantSerialNumber(merchantSerialNumber)
+            .apiV3Key(apiV3Key)
+            .build();
+
+    // 初始化服务
+    service = new H5Service.Builder().config(config).build();
+    // ... 调用接口
+    try {
+      closeOrder();
+    } catch (HttpException e) { // 发送HTTP请求失败
+      // 调用e.getHttpRequest()获取请求打印日志或上报监控，更多方法见HttpException定义
+    } catch (ServiceException e) { // 服务返回状态小于200或大于等于300，例如500
+      // 调用e.getResponseBody()获取返回体打印日志或上报监控，更多方法见ServiceException定义
+    } catch (MalformedMessageException e) { // 服务返回成功，返回体类型不合法，或者解析返回体失败
+      // 调用e.getMessage()获取信息打印日志或上报监控，更多方法见MalformedMessageException定义
+    }
+  }
+
+  /** 关闭订单 */
+  public static void closeOrder() {
+
+    CloseOrderRequest request = new CloseOrderRequest();
+    // 调用request.setXxx(val)设置所需参数，具体参数可见Request定义
+    // 调用接口
+    service.closeOrder(request);
+  }
+
+  /** H5支付下单 */
+  public static PrepayResponse prepay() {
+    PrepayRequest request = new PrepayRequest();
+    // 调用request.setXxx(val)设置所需参数，具体参数可见Request定义
+    // 调用接口
+    return service.prepay(request);
+  }
+
+  /** 商户订单号查询订单 */
+  public static Transaction queryOrderByCombineOutTradeNo() {
+
+    QueryOrderByCombineOutTradeNoRequest request = new QueryOrderByCombineOutTradeNoRequest();
+    // 调用request.setXxx(val)设置所需参数，具体参数可见Request定义
+    // 调用接口
+    return service.queryOrderByCombineOutTradeNo(request);
+  }
+}
---
+++ service/src/main/java/com/wechat/pay/java/service/ecommercecombinepayments/app/AppServiceExtension.java
@@ -0,0 +1,126 @@
+package com.wechat.pay.java.service.ecommercecombinepayments.app;
+
+import com.wechat.pay.java.core.Config;
+import com.wechat.pay.java.core.cipher.Signer;
+import com.wechat.pay.java.core.exception.HttpException;
+import com.wechat.pay.java.core.exception.MalformedMessageException;
+import com.wechat.pay.java.core.exception.ServiceException;
+import com.wechat.pay.java.core.exception.ValidationException;
+import com.wechat.pay.java.core.http.HostName;
+import com.wechat.pay.java.core.http.HttpClient;
+import com.wechat.pay.java.core.util.NonceUtil;
+import com.wechat.pay.java.service.ecommercecombinepayments.app.model.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Instant;
+
+/**
+ * APP 支付的扩展类。
+ *
+ * <p>它封装了 AppService，并提供了一个增强的 APP 下单方法 prepayWithRequestPayment。
+ */
+public class AppServiceExtension {
+  private final Signer signer;
+  private final AppService appService;
+  private static final Logger logger = LoggerFactory.getLogger(AppServiceExtension.class);
+
+  private AppServiceExtension(Config config, HttpClient httpClient, HostName hostName) {
+    this.signer = config.createSigner();
+    AppService.Builder builder = new AppService.Builder().config(config);
+    if (httpClient != null) {
+      builder.httpClient(httpClient);
+    }
+    if (hostName != null) {
+      builder.hostName(hostName);
+    }
+    this.appService = builder.build();
+  }
+
+  /**
+   * APP 支付下单，并返回 APP 调起支付数据。推荐使用！
+   *
+   * <p>请求成功后，该方法返回预支付交易会话标识 prepay_id 和客户端 APP 调起支付所需参数。 它相比 AppService.prepay
+   * 更简单易用，因为无需开发者自行计算调起支付签名。
+   *
+   * @param request 请求参数
+   * @param requestPaymentAppid 商户申请的公众号对应的appid
+   * @return PrepayWithRequestPaymentResponse
+   * @throws HttpException 发送HTTP请求失败。例如构建请求参数失败、发送请求失败、I/O错误等。包含请求信息。
+   * @throws ValidationException 发送HTTP请求成功，验证微信支付返回签名失败。
+   * @throws ServiceException 发送HTTP请求成功，服务返回异常。例如返回状态码小于200或大于等于300。
+   * @throws MalformedMessageException 服务返回成功，content-type不为application/json、解析返回体失败。
+   */
+  public PrepayWithRequestPaymentResponse prepayWithRequestPayment(
+          PrepayRequest request, String requestPaymentAppid) {
+    String prepayId = appService.prepay(request).getPrepayId();
+    long timestamp = Instant.now().getEpochSecond();
+    String nonceStr = NonceUtil.createNonce(32);
+    String message =
+        requestPaymentAppid + "\n" + timestamp + "\n" + nonceStr + "\n" + prepayId + "\n";
+    logger.debug("Message for RequestPayment signatures is[{}]", message);
+    String sign = signer.sign(message).getSign();
+    PrepayWithRequestPaymentResponse response = new PrepayWithRequestPaymentResponse();
+    response.setAppid(requestPaymentAppid);
+    response.setPartnerId(request.getCombineMchid());
+    response.setPrepayId(prepayId);
+    response.setPackageVal("Sign=WXPay");
+    response.setNonceStr(nonceStr);
+    response.setTimestamp(String.valueOf(timestamp));
+    response.setSign(sign);
+    return response;
+  }
+
+  /**
+   * 商户订单号查询订单
+   *
+   * @param request 请求参数
+   * @return Transaction
+   * @throws HttpException 发送HTTP请求失败。例如构建请求参数失败、发送请求失败、I/O错误等。包含请求信息。
+   * @throws ValidationException 发送HTTP请求成功，验证微信支付返回签名失败。
+   * @throws ServiceException 发送HTTP请求成功，服务返回异常。例如返回状态码小于200或大于等于300。
+   * @throws MalformedMessageException 服务返回成功，content-type不为application/json、解析返回体失败。
+   */
+  public Transaction queryOrderByCombineOutTradeNo(QueryOrderByCombineOutTradeNoRequest request) {
+    return appService.queryOrderByCombineOutTradeNo(request);
+  }
+
+  /**
+   * 关闭订单
+   *
+   * @param request 请求参数
+   * @throws HttpException 发送HTTP请求失败。例如构建请求参数失败、发送请求失败、I/O错误等。包含请求信息。
+   * @throws ValidationException 发送HTTP请求成功，验证微信支付返回签名失败。
+   * @throws ServiceException 发送HTTP请求成功，服务返回异常。例如返回状态码小于200或大于等于300。
+   * @throws MalformedMessageException 服务返回成功，content-type不为application/json、解析返回体失败。
+   */
+  public void closeOrder(CloseOrderRequest request) {
+    appService.closeOrder(request);
+  }
+
+  public static class Builder {
+    private Config config;
+
+    private HttpClient httpClient;
+    private HostName hostName;
+
+    public Builder config(Config config) {
+      this.config = config;
+      return this;
+    }
+
+    public Builder httpClient(HttpClient httpClient) {
+      this.httpClient = httpClient;
+      return this;
+    }
+
+    public Builder hostName(HostName hostName) {
+      this.hostName = hostName;
+      return this;
+    }
+
+    public AppServiceExtension build() {
+      return new AppServiceExtension(config, httpClient, hostName);
+    }
+  }
+}
